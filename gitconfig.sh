58f6860d898a3bee24fdb8562dfa3e767f4e49bf
sron <sronhuang@gjsoft.net>
2019-01-16 21:22:27 +0800

    initial commit

---
+++ gitconfig.sh
@@ -0,0 +1,13 @@
+# 設定預設 Git Alias
+git config --global alias.co   checkout
+git config --global alias.ci   commit
+git config --global alias.st   status
+git config --global alias.sts  "status -s"
+git config --global alias.br   branch
+git config --global alias.re   remote
+git config --global alias.di   diff
+git config --global alias.type "cat-file -t"
+git config --global alias.dump "cat-file -p"
+git config --global alias.lo   "log --oneline"
+git config --global alias.ll   "log --pretty=format:'%h %ad | %s%d [%Cgreen%an%Creset]' --graph --date=short"
+git config --global alias.lg   "log --graph --pretty=format:'%Cred%h%Creset %ad |%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%Cgreen%an%Creset]' --abbrev-commit --date=short"
